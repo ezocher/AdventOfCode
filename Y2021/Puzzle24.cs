51469eeb3b9e2111b35f1930b649d59193f70ae7
Eric Zocher <ezocher@users.noreply.github.com>
2022-03-25 16:00:46 -0700

    Solve Puzzle 24 Part 1

--- Y2021/Puzzle24.cs
+++ Y2021/Puzzle24.cs
@@ -15,27 +15,17 @@ namespace AdventOfCode.Y2021
 
         const int NumInputDigits = 14;
 
+        const int MinW = 1, MaxW = 9;
+
         // Extracted from puzzle24--.txt using ExtractCoefficientsFromPuzzleInput() in Puzzle24Original.cs
-        static readonly int[] A = new int[] { 1, 1, 1, 1, 26, 1, 1, 26, 1, 26, 26, 26, 26, 26 };
+        static readonly int[] A = new int[] {  1,  1,  1,  1,  26,  1,  1, 26,  1, 26, 26, 26,  26, 26 };
         static readonly int[] B = new int[] { 12, 11, 10, 10, -16, 14, 12, -4, 15, -7, -8, -4, -15, -8 };
-        static readonly int[] C = new int[] { 6, 12, 5, 10, 7, 0, 4, 12, 14, 13, 10, 11, 9, 9 };
-        public override void Setup() { }
+        static readonly int[] C = new int[] {  6, 12,  5, 10,   7,  0,  4, 12, 14, 13, 10, 11,   9,  9 };
+        // In stages where A = 1 and B >= 10, z always increases ("Up stages")
+        // In stages where A = 26 and B is negative, z can decrease depending on value of w ("Down stages")
+        // There are 7 up stages and 7 down stages, so down stages must always decrease z (TODO: confirm, may only need 6 down)
 
-        Queue<(long, long, int)> candidates;
-        bool found;
-        long largestSerial;
-        private void ReverseSearch(long z, long digits, int stage)
-        {
-            if (stage == 13)
-            {
-                for (int w = 9; w >= 1; w--)
-                {
-                    int z0 = w - B[stage];
-                    candidates.Enqueue((z0, w, stage - 1));
-                }
-            }
-        }
-
+        // Run from stage 0 up to 13 - No longer used
         private long NextZ(long z, int w, int stage)
         {
             long z1 = z / A[stage];
@@ -45,10 +35,88 @@ namespace AdventOfCode.Y2021
                 return z1 * 26 + w + C[stage];
         }
 
+        // Run from stage 13 down to stage 0
+        private List<(long, int)> PreviousZ(long z, int stage)
+        {
+            List<(long, int)> pairs = new();
+
+            long adjustedZ, previousZ;
+
+            if (B[stage] > MaxW)
+            {
+                // Up stage - B doesn't come into play because the if determined that the w == z % 26 + B is always false
+                // This always finds no pairs or 1 pair
+                adjustedZ = z - C[stage];
+                previousZ = adjustedZ / 26;  // this is also * A[stage], but A is always 1 when B > MaxW in my dataset (all datasets?)
+                int w = (int)(adjustedZ % 26);
+                if ((w >= MinW) && (w <= MaxW))
+                    pairs.Add((previousZ, w));
+            }
+            else
+            {
+                // Down stage - assume for now that all down stages must be triggered to lower the value of z,
+                //   so w == z % 26 + B is always true, which means C doesn't come into play because
+                //   C * x is always 0 because x is always 0
+                // This always finds 9 pairs
+                for (int w = MinW; w <= MaxW ; w++)
+                {
+                    previousZ = z * 26 + w - B[stage];
+                    pairs.Add((previousZ, w));
+                }
+            }
+            return pairs;
+        }
+
+        private void DisplayZWPairs(List<(long, int)> pairs, long z, int stage)
+        {
+            if (pairs.Count == 0)
+                Console.WriteLine($"z = {z}, stage = {stage} - no solutions");
+            else if (pairs.Count == 1)
+                Console.WriteLine($"z = {z}, stage = {stage} -> previous z = {pairs[0].Item1} when w = {pairs[0].Item2}");
+            else
+            {
+                Console.WriteLine($"z = {z}, stage = {stage} - {pairs.Count} solutions:");
+                foreach (var pair in pairs)
+                    Console.WriteLine($"    previous z = {pair.Item1} when w = {pair.Item2}");
+            }
+        }
+
+        Stack<(string, long, int)> candidates; // digits, z, nextStage
+        bool found;
+        string firstSolution;
+
+        public override void Setup() { }
+
+        private void ReverseSearch(string digits, long z, int stage)
+        {
+            List<(long, int)> zwPairs;
+            zwPairs = PreviousZ(z, stage);
+
+            // DisplayZWPairs(zwPairs, z, stage);
+
+            foreach (var zwPair in zwPairs)
+            {
+                (long previousZ, int w) = zwPair;
+
+                if (stage == 0)
+                {
+                    if (previousZ == 0)
+                    {
+                        found = true;
+                        firstSolution = w.ToString() + digits;
+                        Console.WriteLine($"*** solution = {firstSolution}");
+                        return;
+                    }
+                }
+                else
+                    candidates.Push((w.ToString() + digits, previousZ, stage - 1));
+            }
+        }
+
+        // No longer used
         const int LastStage = 13;
         static long lowestZ;
         static long firstZero;
-
         private void DepthFirst(int stage, long z, long parent)
         {
             if (firstZero > 0) return;
@@ -72,45 +140,29 @@ namespace AdventOfCode.Y2021
             }
         }
 
+        private bool largestFirst;
+
         [Description("What is the largest model number accepted by MONAD?")]
         public override string SolvePart1()
         {
-            //lowestZ = long.MaxValue;
-            //firstZero = 0;
+            found = false;
+            candidates = new();
+            largestFirst = true;
 
-            //DepthFirst(0, 0, 0);
+            candidates.Push(("", 0, LastStage));
 
-            //found = false;
-            //candidates = new();
-            //candidates.Enqueue((0, 13, 0));
+            while ((!found) && (candidates.Count > 0))
+            {
+                (string digits, long z, int stage) = candidates.Pop();
+                ReverseSearch(digits, z, stage);
+            }
 
-            //while ((!found) && (candidates.Count > 0))
-            //{
-            //    (long z, long digits, int stage) = candidates.Dequeue();
-            //    ReverseSearch(z, digits, stage);
-            //}
-
-            //long z = 0;
-            int stage = 1;
-            for (long z = 7; z <= 15; z++)
-                for (int w = 1; w <= 9; w++)
-                    Console.WriteLine($"stage = {stage}, w = {z-6}{w}, z = {z}, next z = {NextZ(z, w, stage)}");
-            
-            //Random rand = new();
-            //for (int stage = 0; stage <= 13; stage++)
-            //{
-            //    int w = rand.Next(9) + 1;
-            //    Console.WriteLine($"stage = {stage}, w = {w}, next z = {z = NextZ(z, w, stage)}");
-            //}
-            
-            return largestSerial.ToString(); //  LargestValidSerialNumber().ToString();
+            return firstSolution.ToString();
         }
 
-        [Description("What is the largest model number accepted by MONAD?")]
+        [Description("What is the smallest model number accepted by MONAD?")]
         public override string SolvePart2()
         {
-            //Setup();    // Remove if Part 2 builds on output of Part 1
-
             return string.Empty;
         }
     }
