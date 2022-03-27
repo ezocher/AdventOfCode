f90f6a5aa8aec0d836e0b706dca5b19ec84cf92e
Eric Zocher <ezocher@users.noreply.github.com>
2022-03-27 15:07:32 -0700

    Finish skeleton of Puzzle 19

--- Y2021/Puzzle19.cs
+++ Y2021/Puzzle19.cs
@@ -23,7 +23,22 @@ namespace AdventOfCode.Y2021
                 X = x; Y = y; Z = z;
             }
 
+            public IntPoint3D ApplyTransform(Transform transform)
+            {
+                return new(this.X, this.Y, this.Z);
+            }
+
             public override string ToString() => $"{X},{Y},{Z}";
+        }
+
+        struct Transform
+        {
+            public int I;
+
+            public Transform(int i)
+            {
+                I = i;
+            }
         }
 
         struct BeaconPair
@@ -54,10 +69,49 @@ namespace AdventOfCode.Y2021
             public override string ToString() => $"{Distance:F2}: [{IndexA}] <=> [{IndexB}]";
         }
 
+        class Scanner
+        {
+            public List<IntPoint3D> Beacons;
+            public List<BeaconPair> BeaconPairs;
+            public List<ScannerPair> OverlappingScanners;
+            public bool OverlappingScannersRemapped = false;
+            public int Index;
+
+            // Direction
+
+            // Beacon Distance Map
+
+            public Scanner(List<IntPoint3D> beacons, int index)
+            {
+                Beacons = beacons;
+                Index = index;
+
+                BeaconPairs = new();
+                for (int a = 0; a < Beacons.Count; a++)
+                    for (int b = a + 1; b < Beacons.Count; b++)
+                        BeaconPairs.Add(new BeaconPair(a, b, Beacons[a], Beacons[b]));
+                BeaconPairs.Sort((s1, s2) => s1.Distance.CompareTo(s2.Distance));
+
+                OverlappingScanners = new();
+            }
+
+            public void AddBeaconsToMasterList(HashSet<string> masterList)
+            {
+                foreach (IntPoint3D b in this.Beacons)
+                    masterList.Add(b.ToString());
+            }
+
+            public void TransformAllBeacons(Transform transform)
+            {
+                for (int i = 0; i < Beacons.Count; i++)
+                    Beacons[i] = Beacons[i].ApplyTransform(transform);
+            }
+        }
+
         struct ScannerPair
         {
-            int IndexAlpha;
-            int IndexBeta;
+            public int IndexAlpha;
+            public int IndexBeta;
             bool[] MatchedBeaconsAlpha;
             bool[] MatchedBeaconsBeta;
             List<(int, int, int, int)> MatchedBeaconPairsIndexes;
@@ -101,34 +155,11 @@ namespace AdventOfCode.Y2021
             public override string ToString() => $"Scanners {IndexAlpha} & {IndexBeta}: {MatchedBeaconCount} matches";
         }
 
-        class Scanner
-        {
-            public List<IntPoint3D> Beacons;
-            public List<BeaconPair> BeaconPairs;
-            public List<int> OverlappingScannerIndexes;
-
-            // Direction
-
-            // Beacon Distance Map
-
-            public Scanner(List<IntPoint3D> beacons)
-            {
-                Beacons = beacons;
-
-                BeaconPairs = new();
-                for (int a = 0; a < Beacons.Count; a++)
-                    for (int b = a + 1; b < Beacons.Count; b++)
-                        BeaconPairs.Add(new BeaconPair(a, b, Beacons[a], Beacons[b]));
-                BeaconPairs.Sort((s1, s2) => s1.Distance.CompareTo(s2.Distance));
-
-                OverlappingScannerIndexes = new();
-            }
-        }
-
         private List<Scanner> scanners;
-        private bool[] mappedScanners;
-        private List<ScannerPair> overlappingScannerPairs;
+        private bool[] remappedScanners;
         private HashSet<string> recordedBeacons;
+
+        private const int CoordinateSystemBaseScannerIndex = 0;
 
         public Puzzle19(string input) : base(input) { Name = "Beacon Scanner"; }
 
@@ -138,25 +169,25 @@ namespace AdventOfCode.Y2021
 
             scanners = new();
             List<IntPoint3D> beacons = new();
+            int scannerIndex = 0;
 
             for (int i = 1; i < lines.Count; i++)
             {
                 if (lines[i].StartsWith("--- scanner"))
                 {
-                    scanners.Add(new Scanner(beacons));
+                    scanners.Add(new Scanner(beacons, scannerIndex++));
                     beacons = new();
                 }
                 else if (lines[i] != string.Empty)
                     beacons.Add(new IntPoint3D(lines[i]));
             }
-            scanners.Add(new Scanner(beacons));
+            scanners.Add(new Scanner(beacons, scannerIndex));
 
-            overlappingScannerPairs = new();
-            mappedScanners = new bool[scanners.Count];
+            remappedScanners = new bool[scanners.Count];
             recordedBeacons = new();
         }
 
-        private void CompareScanners()
+        private void FindOverlappingScannerPairs()
         {
             const int MatchThreshold = 12;
 
@@ -166,24 +197,64 @@ namespace AdventOfCode.Y2021
                     ScannerPair sp = new ScannerPair(a, b, scanners[a], scanners[b]);
                     if (sp.MatchedBeaconCount >= MatchThreshold)
                     {
-                        overlappingScannerPairs.Add(sp);
-                        scanners[a].OverlappingScannerIndexes.Add(b);
-                        scanners[b].OverlappingScannerIndexes.Add(a);
+                        scanners[a].OverlappingScanners.Add(sp);
+                        scanners[b].OverlappingScanners.Add(sp);
                     }
                 }
         }
 
-        private void AddBeacons(Scanner s)
+        private void RemapCoordinates(ScannerPair sp, Scanner baseScanner)
         {
-            foreach (IntPoint3D b in s.Beacons)
-                recordedBeacons.Add(b.ToString());
+            bool alphaIsBaseScanner = (sp.IndexAlpha == baseScanner.Index); 
+            Scanner remapScanner = alphaIsBaseScanner ? scanners[sp.IndexBeta] : scanners[sp.IndexAlpha];
+
+            if (!remappedScanners[remapScanner.Index])
+            {
+                Transform transform = FindOffsetAndRotation(baseScanner, remapScanner);
+
+                remapScanner.TransformAllBeacons(transform);
+
+                remappedScanners[remapScanner.Index] = true;
+            }
         }
+
+        private Transform FindOffsetAndRotation(Scanner baseScanner, Scanner remapScanner)
+        {
+            return new Transform(666);
+        }
+
+        private void RemapAllScannerCoordinates(Scanner startingScanner)
+        {
+            Scanner baseScanner = startingScanner;
+
+            while (Tools.CountTrue(remappedScanners) < scanners.Count)
+            {
+                foreach (var scannerPair in baseScanner.OverlappingScanners)
+                    RemapCoordinates(scannerPair, baseScanner);
+                baseScanner.OverlappingScannersRemapped = true;
+
+                int i = 0;
+                while (!remappedScanners[i] || scanners[i].OverlappingScannersRemapped)
+                    i++;
+                baseScanner = scanners[i];
+            }
+        }
+
 
         [Description("How many beacons are there?")]
         public override string SolvePart1()
         {
-            CompareScanners();
-            //AddBeacons(scanners[0]);
+            // Setup() reads scanners from input and Scanner() constructor builds beacon pairs
+
+            // Use a specific scanner as coordinate system base
+            remappedScanners[CoordinateSystemBaseScannerIndex] = true;
+
+            FindOverlappingScannerPairs();
+
+            RemapAllScannerCoordinates(scanners[CoordinateSystemBaseScannerIndex]);
+
+            foreach (var scanner in scanners)   
+                scanner.AddBeaconsToMasterList(recordedBeacons);
 
             return recordedBeacons.Count.ToString();
         }
